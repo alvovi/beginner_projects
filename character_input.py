24aa2d8e4f7050f330ca4299c835f2308f82e099
Anna Lvova <44921065+alvovi@users.noreply.github.com>
2018-11-13 19:06:10 +0000

    Create character_input.py
    Character Input project from practisepython.org

---
+++ character_input.py
@@ -0,0 +1,7 @@
+name = input("Hi! What is your name?")
+age = int(input(f"And how old are you, {name}?"))
+number = int(input("Pick a number!"))
+print()
+year = 2018 + 100 - age
+for x in range(0, number):
+    print(f"You will be 100 in {year}!\n")
